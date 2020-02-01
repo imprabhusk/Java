3ebeceb21f5817f113babb7ff83e26c3d3786f97
prabhusk <prabhu.sk.169@gmail.com>
2020-02-01 20:11:24 +0530

    Fibonacci added.

--- Basics/Fibonacci.java
+++ Basics/Fibonacci.java
@@ -1,6 +1,8 @@
-// Java Program to Generate the Fibonacci Series upto the Given Number.
+// Java Program to Generate the Fibonacci Series upto the Limit.
 
 import java.util.Scanner;
+
+// Method - 1
 
 public class Fibonacci {
 
@@ -20,6 +22,34 @@ public class Fibonacci {
             first = second;
             second = result;
         }
+
+        input.close();
+    }
+}
+
+// Method - 2 (Using Methods)
+
+public class Fibonacci {
+    public static void generateFibonacci(int first, int second, int limit){
+        for(int i = 1; i <= limit; i++){
+            int result = first + second;
+            System.out.print(result + " ");
+            first = second;
+            second = result;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the First Number : ");
+        int first = input.nextInt();
+        System.out.print("Enter the Second Number : ");
+        int second = input.nextInt();
+        System.out.print("Enter the Number To Generate Fibonacci Series : ");
+        int limit = input.nextInt();
+
+        generateFibonacci(first, second, limit);
+
         input.close();
     }
 }
