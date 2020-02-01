016ba445fce62517547ff748d3b0bd55cd540aec
prabhusk <prabhu.sk.169@gmail.com>
2020-02-01 17:28:53 +0530

    Factorial Program  added.

--- Basics/Factorial.java
+++ Basics/Factorial.java
@@ -4,20 +4,20 @@ import java.util.Scanner;
 
 // Method - 1
 
-// public class Factorial {
+public class Factorial {
 
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         System.out.print("Enter the Number to Find Factorial : ");
-//         int number = input.nextInt();
-//         int fact = 1;
-//         for(int i = 1 ; i <= number ; i++){
-//             fact *= i;
-//         }
-//         System.out.println("Factorial of the Number " + number + " is " + fact);
-//         input.close();
-//     }
-// }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Factorial : ");
+        int number = input.nextInt();
+        int fact = 1;
+        for(int i = 1 ; i <= number ; i++){
+            fact *= i;
+        }
+        System.out.println("Factorial of the Number " + number + " is " + fact);
+        input.close();
+    }
+}
 
 // Method - 2 (Using Methods)
 
@@ -41,23 +41,23 @@ import java.util.Scanner;
 //     }
 // }
 
-// Method - 3 (Using Recursion Method)
+// // Method - 3 (Using Recursion Method)
 
-public class Factorial {
+// public class Factorial {
 
-    public static void factorial(int number) {
-        int fact = 1;
-        int temp = number;
-        while(number > 0){
-            fact *= number--;
-        }
-        System.out.println("Factorial of the Number " + temp + " is " + fact);
-    }
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Number to Find Factorial : ");
-        int number = input.nextInt();
-        factorial(number);
-        input.close();
-    }
-}
+//     public static void factorial(int number) {
+//         int fact = 1;
+//         int temp = number;
+//         while(number > 0){
+//             fact *= number--;
+//         }
+//         System.out.println("Factorial of the Number " + temp + " is " + fact);
+//     }
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter the Number to Find Factorial : ");
+//         int number = input.nextInt();
+//         factorial(number);
+//         input.close();
+//     }
+// }
