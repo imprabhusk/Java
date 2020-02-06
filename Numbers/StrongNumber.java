b73b673c305103d04d744d43b2492f42cd1e576c
prabhusk <prabhu.sk.169@gmail.com>
2020-02-06 22:01:28 +0530

    Strong Number Program added.

--- Numbers/StrongNumber.java
+++ Numbers/StrongNumber.java
@@ -14,8 +14,27 @@ import java.util.Scanner;
 // Method - 1
 
 public class StrongNumber {
+    public static int factorial(int number){
+        int fact = 1;
+        for(int i = 0; i <= number; i++){
+            fact = fact * i;
+        }
+        return fact;
+    }
 
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Strong Number Or Not : ");
+        int number = input.nextInt();
+        int sum = 0;
         
+        while(number > 0){
+            int result = number % 10;
+            sum += factorial(result);
+            number /= 10;
+        }
+
+        System.out.println(number == sum ? "Strong Number" : "Not Strong Number");
+        input.close();
     }
 }
