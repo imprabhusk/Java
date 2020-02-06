83875c0488181fdc2b86db5619f1bbbab2c453c5
prabhusk <prabhu.sk.169@gmail.com>
2020-02-06 21:50:42 +0530

    Strong Number Program added.

--- Numbers/StrongNumber.java
+++ Numbers/StrongNumber.java
@@ -2,3 +2,9 @@
 
 // Strong Number:
 // The Sum of the Factorial of its digits is equal to the number itself
+
+// Example:
+// 145 = 1! + 4! + 5!
+// 145 = 1 + (4 * 3 * 2 * 1) + (5 * 4 * 3 * 2 * 1)
+// 145 = 1 + 24 + 120
+// 145 = 145
