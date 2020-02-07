5897644f4e707f2360fa3fb98876bd36a40ae54f
prabhusk <prabhu.sk.169@gmail.com>
2020-02-07 14:40:29 +0530

    Hello World Program using Different ways Added.

--- Basics/Hello.java
+++ Basics/Hello.java
@@ -1,8 +1,21 @@
 // Simple Hello World Program in Java
 
+// Valid Ways to Write Hello World Program in Java
+
+// Method - 1 Using String[] args
+
 public class Hello {
 
     public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// Method - 2 Using String []args
+
+public class Hello {
+
+    public static void main(String []args) {
         System.out.println("Hello World!");
     }
 }
