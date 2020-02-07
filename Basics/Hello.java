fe3070b907dbbceca57340f5649daab2fb467023
prabhusk <prabhu.sk.169@gmail.com>
2020-02-07 14:57:11 +0530

    Hello World Program using Different ways Added.

--- Basics/Hello.java
+++ Basics/Hello.java
@@ -4,73 +4,90 @@
 
 // Method - 1 Using String[] args
 
-public class Hello {
+// public class Hello {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-}
+//     public static void main(String[] args) {
+//         System.out.println("Hello World!");
+//     }
+// }
 
 // Method - 2 Using String []args
 
-public class Hello {
+// public class Hello {
 
-    public static void main(String []args) {
-        System.out.println("Hello World!");
-    }
-}
+//     public static void main(String []args) {
+//         System.out.println("Hello World!");
+//     }
+// }
 
 // Method - 3 Using String args[]
 
-public class Hello {
+// public class Hello {
 
-    public static void main(String args[]) {
-        System.out.println("Hello World!");
-    }
-}
+//     public static void main(String args[]) {
+//         System.out.println("Hello World!");
+//     }
+// }
 
 // Method - 4 Using String... args
 
-public class Hello {
+// public class Hello {
 
-    public static void main(String... args) {
-        System.out.println("Hello World!");
-    }
-}
+//     public static void main(String... args) {
+//         System.out.println("Hello World!");
+//     }
+// }
 
 // Method - 5 By Changing the places of Access Specifier and Access Modifier
 
-public class Hello {
+// public class Hello {
 
-    static public void main(String... args) {
-        System.out.println("Hello World!");
-    }
-}
+//     static public void main(String... args) {
+//         System.out.println("Hello World!");
+//     }
+// }
 
 // Method - 6 Using Final Keyword in main method
 
-public class Hello {
+// public class Hello {
 
-    public static final void main(String... args) {
-        System.out.println("Hello World!");
-    }
-}
+//     public static final void main(String... args) {
+//         System.out.println("Hello World!");
+//     }
+// }
 
 // Method - 7 Using Final keyword in first place
 
-public class Hello {
+// public class Hello {
 
-    final public static void main(String... args) {
-        System.out.println("Hello World!");
-    }
-}
+//     final public static void main(String... args) {
+//         System.out.println("Hello World!");
+//     }
+// }
 
 // Method - 8 Using Strictfp keyword
 
+// public class Hello {
+
+//     final strictfp public static void main(String... args) {
+//         System.out.println("Hello World!");
+//     }
+// }
+
+// Invalid Ways to Write Hello World Program in Java
+
+// Method - 1 (Without using static keyword)
+
 public class Hello {
 
-    final strictfp public static void main(String... args) {
+    public void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
 
+// output : 
+
+// This program compiles successfully but produce an error which is
+
+// Error: Main method is not static in class Hello, please define the main method as:
+//    public static void main(String[] args)
