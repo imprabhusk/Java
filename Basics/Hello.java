67f9d62ad21e40fd05e87092ab969a58a0536840
prabhusk <prabhu.sk.169@gmail.com>
2020-02-07 15:11:49 +0530

    Hello World Program using valid and invalid  ways Added.

--- Basics/Hello.java
+++ Basics/Hello.java
@@ -4,86 +4,102 @@
 
 // Method - 1 Using String[] args
 
-// public class Hello {
+public class Hello {
 
-//     public static void main(String[] args) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output : Hello World!
 
 // Method - 2 Using String []args
 
-// public class Hello {
+public class Hello {
 
-//     public static void main(String []args) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    public static void main(String []args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output : Hello World!
 
 // Method - 3 Using String args[]
 
-// public class Hello {
+public class Hello {
 
-//     public static void main(String args[]) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    public static void main(String args[]) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output : Hello World!
 
 // Method - 4 Using String... args
 
-// public class Hello {
+public class Hello {
 
-//     public static void main(String... args) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    public static void main(String... args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output : Hello World!
 
 // Method - 5 By Changing the places of Access Specifier and Access Modifier
 
-// public class Hello {
+public class Hello {
 
-//     static public void main(String... args) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    static public void main(String... args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output : Hello World!
 
 // Method - 6 Using Final Keyword in main method
 
-// public class Hello {
+public class Hello {
 
-//     public static final void main(String... args) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    public static final void main(String... args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output : Hello World!
 
 // Method - 7 Using Final keyword in first place
 
-// public class Hello {
+public class Hello {
 
-//     final public static void main(String... args) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    final public static void main(String... args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output : Hello World!
 
 // Method - 8 Using Strictfp keyword
 
-// public class Hello {
+public class Hello {
 
-//     final strictfp public static void main(String... args) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    final strictfp public static void main(String... args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output : Hello World!
 
 // Invalid Ways to Write Hello World Program in Java
 
 // Method - 1 (Without using static keyword)
 
-// public class Hello {
+public class Hello {
 
-//     public void main(String[] args) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    public void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
 
 // output : 
 
@@ -94,16 +110,51 @@
 
 // Method - 2 (Without using public keyword)
 
-// public class Hello {
+public class Hello {
 
-//     static void main(String[] args) {
-//         System.out.println("Hello World!");
-//     }
-// }
+    static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
 
 // output :
 
 // This program compiles successfully but produce an error at runtime.
+
 // Error: Main method not found in class Hello, please define the main method as:
 //    public static void main(String[] args)
 // or a JavaFX application class must extend javafx.application.Application
+
+// Method - 3 (Interchanging the Access Modifier(static) and Datatype or returntype(void))
+
+public class Hello {
+
+    public void static main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output :
+
+// error: <identifier> expected
+//     public void static main(String[] args) {
+// error: '(' expected
+//     public void static main(String[] args) {
+// error: invalid method declaration; return type required
+//     public void static main(String[] args) {
+
+// Method - 4 (using abstract keyword)
+
+public class Hello {
+
+    abstract public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
+// output :
+
+// error: illegal combination of modifiers: abstract and static
+//     abstract public static void main(String[] args) {
+// error: Hello is not abstract and does not override abstract method main(String[]) in Hello
+//     public class Hello       
