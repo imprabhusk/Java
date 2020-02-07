3634fac9c29c7fc70f3c6ac2d9b4497e45ceaa6e
prabhusk <prabhu.sk.169@gmail.com>
2020-02-07 15:50:32 +0530

    Even Or Odd Using Objects added.

--- Basics/EvenOrOdd.java
+++ Basics/EvenOrOdd.java
@@ -3,7 +3,7 @@
 import java.util.Scanner;
 
 
-// Method - 1 
+// Method - 1 (Using if/else Logic)
 
 public class EvenOrOdd {
 
@@ -11,17 +11,19 @@ public class EvenOrOdd {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the Number To Find Odd Or Even : ");
         int number = input.nextInt();
+
         if(number % 2 == 0){
             System.out.println(number + " is Even Number");
         }
         else {
             System.out.println(number + " is Odd Number");
         }
+
         input.close();
     }
 }
 
-// Method - 2 (Using Ternary Operator method)
+// Method - 2 (Using if/else Logic without Braces)
 
 public class EvenOrOdd {
 
@@ -29,12 +31,32 @@ public class EvenOrOdd {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the Number To Find Odd Or Even : ");
         int number = input.nextInt();
-        System.out.println(number % 2 == 0 ?  (number + " is Even Number") : (number + " is Odd Number"));
+
+        if(number % 2 == 0)
+            System.out.println(number + " is Even Number");
+        else
+            System.out.println(number + " is Odd Number");
+
         input.close();
     }
 }
 
-// Method - 3 (Using Methods)
+// Method - 3 (Using Ternary Operator method)
+
+public class EvenOrOdd {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number To Find Odd Or Even : ");
+        int number = input.nextInt();
+
+        System.out.println(number % 2 == 0 ?  (number + " is Even Number") : (number + " is Odd Number"));
+
+        input.close();
+    }
+}
+
+// Method - 4 (Using Static Method)
 
 public class EvenOrOdd {
 
@@ -46,6 +68,26 @@ public class EvenOrOdd {
         System.out.print("Enter the Number To Find Odd Or Even : ");
         int number = input.nextInt();
         isEvenOrOdd(number);
+        input.close();
+    }
+}
+
+// Method - 5 (Using Object)
+
+public class EvenOrOdd {
+
+    public void isEvenOrOdd(int number){
+        System.out.println(number % 2 == 0 ?  (number + " is Even Number") : (number + " is Odd Number"));
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number To Find Odd Or Even : ");
+        int number = input.nextInt();
+
+        EvenOrOdd evenOdd = new EvenOrOdd();
+
+        evenOdd.isEvenOrOdd(number);
         input.close();
     }
 }
