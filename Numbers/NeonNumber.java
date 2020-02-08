da00792ddc5f8c68e24c5a1e993f26e31336af7e
prabhusk <prabhu.sk.169@gmail.com>
2020-02-08 22:46:28 +0530

    Neon Number added.

--- Numbers/NeonNumber.java
+++ Numbers/NeonNumber.java
@@ -8,3 +8,25 @@
 // 9 = 81
 // 9 = 8 + 1
 // 9 = 9
+
+import java.util.Scanner;
+
+public class NeonNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Neon Number Or Not : ");
+        int number = input.nextInt();
+        int square = number * number;
+        int sum = 0;
+
+        while(square != 0){
+            int result = square % 10;
+            sum += result;
+            square /= 10;
+        }
+
+        System.out.print(number == sum ? number + " is Neon Number" : number + " is Not Neon Number");
+        input.close();
+    }
+}
