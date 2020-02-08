36950c8114e13936c6d87ef870a8a927d47c71f5
prabhusk <prabhu.sk.169@gmail.com>
2020-02-08 23:02:48 +0530

    Neon Number added.

--- Numbers/NeonNumber.java
+++ Numbers/NeonNumber.java
@@ -11,22 +11,50 @@
 
 import java.util.Scanner;
 
+// Method - 1 (Using Logic)
+
+// public class NeonNumber {
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter the Number to Find Neon Number Or Not : ");
+//         int number = input.nextInt();
+//         int square = number * number;
+//         int sum = 0;
+
+//         while(square != 0){
+//             int result = square % 10;
+//             sum += result;
+//             square /= 10;
+//         }
+
+//         System.out.print(number == sum ? number + " is Neon Number" : number + " is Not Neon Number");
+//         input.close();
+//     }
+// }
+
+// Method - 2 (Using static method)
+
 public class NeonNumber {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Number to Find Neon Number Or Not : ");
-        int number = input.nextInt();
+    public static void neon(int number, int sum) {
         int square = number * number;
-        int sum = 0;
 
         while(square != 0){
             int result = square % 10;
             sum += result;
             square /= 10;
         }
-
         System.out.print(number == sum ? number + " is Neon Number" : number + " is Not Neon Number");
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Neon Number Or Not : ");
+        int number = input.nextInt();
+        int sum = 0;
+        
+        neon(number, sum);
         input.close();
     }
 }
