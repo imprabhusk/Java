45c851fe68995fbe062e35bd6ad86343db21d661
prabhusk <prabhu.sk.169@gmail.com>
2020-02-09 20:34:31 +0530

    Automorphic Number added.

--- Numbers/AutomorphicNumber.java
+++ Numbers/AutomorphicNumber.java
@@ -7,3 +7,26 @@
 // 5 = 5 ^ 5 = 25 //Last digit is 5
 // 6 = 6 ^ 6 = 36 //Last digit is 6
 // 25 = 25 * 25 = 325 //Last digit is 25
+
+import java.util.Scanner;
+
+public class AutomorphicNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Automorphic Number Or Not : ");
+        int number = input.nextInt();
+        int result = 0;
+
+        int square = number * number;
+
+        while (square != 0) {
+            result = square % 10;
+            square /= 10;
+        }
+
+        System.out
+                .println(result == number ? number + " is Automorphic Number" : number + " is Not Automorphic Number");
+        input.close();
+    }
+}
