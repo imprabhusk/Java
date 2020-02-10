c71674aa6ee2a91d32b61b781dc338680ec74601
prabhusk <prabhu.sk.169@gmail.com>
2020-02-10 21:56:52 +0530

    Spy Number Program added.

--- Numbers/SpyNumber.java
+++ Numbers/SpyNumber.java
@@ -1,1 +1,11 @@
 // Java Program to Find Number is Spy Number Or Not
+
+// Spy Number :
+// The Sum of its digits is Equal to the Product of its digits.
+
+// For Example :
+// 123 = 1 + 2 + 3 = 6
+// 123 = 1 * 2 * 3 = 6
+
+// 1124 = 1 + 1 + 2 + 4
+// 1124 = 1 * 1 * 2 * 4
