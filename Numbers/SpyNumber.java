8cc5080fab762ddbc380f2ae38f6ee4bb14417a0
prabhusk <prabhu.sk.169@gmail.com>
2020-02-10 22:15:00 +0530

    Spy Number Program added.

--- Numbers/SpyNumber.java
+++ Numbers/SpyNumber.java
@@ -9,3 +9,42 @@
 
 // 1124 = 1 + 1 + 2 + 4
 // 1124 = 1 * 1 * 2 * 4
+
+import java.util.Scanner;
+
+// Method - 1
+public class SpyNumber {
+
+    public static int add(int number, int sum) {
+
+        while (number > 0) {
+            int remainder = number % 10;
+            sum += remainder;
+            number /= 10;
+        }
+        System.out.println("Sum is : " + sum);
+        return sum;
+    }
+
+    public static int multiply(int number, int product) {
+        while (number > 0) {
+            int remainder = number % 10;
+            product *= remainder;
+            number /= 10;
+        }
+        System.out.println("Product is : " + product);
+        return product;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Spy Number Or Not : ");
+        int number = input.nextInt();
+        int sum = 0;
+        int product = 1;
+
+        System.out.println(add(number, sum) == multiply(number, product) ? (number + " is Spy Number")
+                : (number + " is Not Spy Number"));
+        input.close();
+    }
+}
