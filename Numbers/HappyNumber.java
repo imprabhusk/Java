9c217dc77111ba0f7807ed4f7b17e15e34ff599a
prabhusk <prabhu.sk.169@gmail.com>
2020-02-11 19:12:33 +0530

    Happy Number added.

--- Numbers/HappyNumber.java
+++ Numbers/HappyNumber.java
@@ -1,1 +1,12 @@
 // Java Program to Find the Number is Happy Number Or Not
+
+// Happy Number :
+// Sum of Square of the digit (until it becomes single digit) is Equal to 1
+
+// For Example :
+// 23 = 2 ^ 2 + 3 ^ 3
+// 13 = 4 + 9
+// 13 = 1 ^ 1 + 3 ^ 3
+// 10 = 1 + 9
+// 10 = 1 ^ 1 + 0 ^ 0
+// 1
