6571314650f23d654b7e9fdfd9d9dab8a3f0d91c
prabhusk <prabhu.sk.169@gmail.com>
2020-02-13 21:59:01 +0530

    Sunny Number Program Example added.

--- Numbers/SunnyNumber.java
+++ Numbers/SunnyNumber.java
@@ -2,3 +2,8 @@
 
 // Sunny Number :
 // Adding 1 to the given number and square root of it becomes whole number.
+
+// For Example :
+// Let's Take Given Number is 3
+// Adding 1 to it gives 4
+// Then Square Root of it becomes 2 which is Whole Number.
