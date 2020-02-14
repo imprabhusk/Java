1295ac8f870382b5aaaa3320a84ef3900295f3eb
prabhusk <prabhu.sk.169@gmail.com>
2020-02-14 23:10:05 +0530

    Disarium Number static method added.

--- Numbers/DisariumNumber.java
+++ Numbers/DisariumNumber.java
@@ -13,31 +13,64 @@ import java.util.Scanner;
 
 // Method - 1 (Using Logic)
 
+// public class DisariumNumber {
+
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+// System.out.print("Enter the Number to Find Disarium Number Or Not : ");
+// int number = input.nextInt();
+// int sum = 0;
+// int count = 0;
+
+// int temp = number;
+// // To Count number of digits
+// while (temp > 0) {
+// temp /= 10;
+// count++;
+// }
+
+// temp = number;
+// // To calculate sum of the number with its power
+// while (temp > 0) {
+// int remainder = temp % 10;
+// sum += Math.pow(remainder, count--);
+// temp /= 10;
+// }
+
+// System.out.print(sum == number ? number + " is Disarium Number" : number + "
+// is Not Disarium Number");
+// input.close();
+// }
+// }
+
+// Method - 2 (Using static method)
+
 public class DisariumNumber {
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Number to Find Disarium Number Or Not : ");
-        int number = input.nextInt();
-        int sum = 0;
-        int count = 0;
-
+    public static int disarium(int number) {
         int temp = number;
-        // To Count number of digits
+        int count = 0;
         while (temp > 0) {
             temp /= 10;
             count++;
         }
 
         temp = number;
-        // To calculate sum of the number with its power
+        int sum = 0;
         while (temp > 0) {
             int remainder = temp % 10;
             sum += Math.pow(remainder, count--);
             temp /= 10;
         }
+        return sum;
+    }
 
-        System.out.print(sum == number ? number + " is Disarium Number" : number + " is Not Disarium Number");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Disarium Number Or Not : ");
+        int number = input.nextInt();
+
+        System.out.println(
+                disarium(number) == number ? number + " is Disarium Number" : number + " is Not Disarium Number");
         input.close();
     }
 }
