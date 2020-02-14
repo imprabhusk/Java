55b909e78764b99509aff97c5867c392d7a7331c
prabhusk <prabhu.sk.169@gmail.com>
2020-02-14 22:03:00 +0530

    Disarium Number Example added.

--- Numbers/DisariumNumber.java
+++ Numbers/DisariumNumber.java
@@ -3,3 +3,8 @@
 // Disarium Number :
 // Sum of the digits powered with their respective positions is equal to the
 // Number itself
+
+// For Example :
+// 89 = 8 ^ 1 + 9 ^ 2
+// 89 = 8 + 81
+// 89 = 89
