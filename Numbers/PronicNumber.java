6b806a6bfa0acf37162a4f14c77d2b71b62ea5b6
prabhusk <prabhu.sk.169@gmail.com>
2020-02-15 20:09:41 +0530

    Pronic Number Program added.

--- Numbers/PronicNumber.java
+++ Numbers/PronicNumber.java
@@ -6,3 +6,27 @@
 // For Example :
 // 2 * 3 = 6
 // 5 * 6 = 30
+
+import java.util.Scanner;
+
+// Method - 1
+
+public class PronicNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Pronic Number Or Not : ");
+        int number = input.nextInt();
+        boolean flag = false;
+        int n = 1;
+
+        for (int i = 1; i <= number; i++) {
+            if (n * (n + 1) == number)
+                flag = true;
+            n++;
+        }
+
+        System.out.println(flag == true ? number + " is Pronic Number" : number + " is Not a Pronic Number");
+        input.close();
+    }
+}
