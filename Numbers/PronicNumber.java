2e3371e578b74fa45c4d48c2d4e63a50ab0ed69e
prabhusk <prabhu.sk.169@gmail.com>
2020-02-15 20:14:04 +0530

    Pronic Number static method added.

--- Numbers/PronicNumber.java
+++ Numbers/PronicNumber.java
@@ -11,13 +11,32 @@ import java.util.Scanner;
 
 // Method - 1
 
+// public class PronicNumber {
+
+// public static void main(String[] args) {
+// Scanner input = new Scanner(System.in);
+// System.out.print("Enter the Number to Find Pronic Number Or Not : ");
+// int number = input.nextInt();
+// boolean flag = false;
+// int n = 1;
+
+// for (int i = 1; i <= number; i++) {
+// if (n * (n + 1) == number)
+// flag = true;
+// n++;
+// }
+
+// System.out.println(flag == true ? number + " is Pronic Number" : number + "
+// is Not a Pronic Number");
+// input.close();
+// }
+// }
+
+// Method - 2 (Using static Method)
+
 public class PronicNumber {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Number to Find Pronic Number Or Not : ");
-        int number = input.nextInt();
-        boolean flag = false;
+    public static boolean isPronic(int number, boolean flag) {
         int n = 1;
 
         for (int i = 1; i <= number; i++) {
@@ -26,7 +45,17 @@ public class PronicNumber {
             n++;
         }
 
-        System.out.println(flag == true ? number + " is Pronic Number" : number + " is Not a Pronic Number");
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Pronic Number Or Not : ");
+        int number = input.nextInt();
+        boolean flag = false;
+
+        System.out.println(
+                isPronic(number, flag) == true ? number + " is Pronic Number" : number + " is Not a Pronic Number");
         input.close();
     }
 }
