2792837a22a9e4d2480a97cddb7ce1ec8a0aac89
prabhusk <prabhu.sk.169@gmail.com>
2020-02-16 21:39:52 +0530

    Trimorphic Number Explanation added.

--- Numbers/TrimorphicNumber.java
+++ Numbers/TrimorphicNumber.java
@@ -2,3 +2,8 @@
 
 // Trimorphic Number :
 // Cube of the Number ends with the Number itsef
+
+
+// For Example :
+// 4 = 4 * 4 * 4 = 64       //Number ends with 4
+// 25 = 25 * 25 * 25 = 625  //Number ends with 25
