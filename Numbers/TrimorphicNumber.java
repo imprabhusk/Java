09eaf3611711c75ee5e7e13e97229bffcfdcf819
prabhusk <prabhu.sk.169@gmail.com>
2020-02-16 22:02:07 +0530

    Trimorphic Number Logic added.

--- Numbers/TrimorphicNumber.java
+++ Numbers/TrimorphicNumber.java
@@ -7,3 +7,32 @@
 // For Example :
 // 4 = 4 * 4 * 4 = 64       //Number ends with 4
 // 25 = 25 * 25 * 25 = 625  //Number ends with 25
+
+import java.util.Scanner;
+
+// Method - 1 (Using Logic)
+
+public class TrimorphicNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Trimorphic Or Not : ");
+        int number = input.nextInt();
+        int temp = number;
+        boolean flag = true;
+
+        int cube = (int) Math.pow(number, 3);
+
+        while(number != 0){
+            if(number % 10 != cube % 10){
+                flag = false;
+                break;
+            }
+            number /= 10;
+            cube /= 10;
+        }
+        
+        System.out.println(flag ? temp + " is Trimorphic Number" : temp + " is Non Trimorphic Number");
+        input.close();
+    }
+}
