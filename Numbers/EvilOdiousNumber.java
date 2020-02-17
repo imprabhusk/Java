74209e2b658aeade46b25c689b884bd294be33bf
prabhusk <prabhu.sk.169@gmail.com>
2020-02-17 22:26:43 +0530

    Evil Odious example added.

--- Numbers/EvilOdiousNumber.java
+++ Numbers/EvilOdiousNumber.java
@@ -3,3 +3,7 @@
 // EvilOrOdiousNumber
 // Convert the given Number into binary format, If the binary representation of the number contains even 
 // number of 1's then its evil number or if contains odd number of 1's then its odious number
+
+// For Example :
+// 3 = 011 = Even number of 1's so its Evil Number
+// 16 = 10000 = Odd number of 1's so its Odious Number
