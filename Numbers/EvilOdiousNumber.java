5091fad5d867e20f102c315e8710bc53d6f04e4e
prabhusk <prabhu.sk.169@gmail.com>
2020-02-17 22:24:32 +0530

    Evil Odious explanation added.

--- Numbers/EvilOdiousNumber.java
+++ Numbers/EvilOdiousNumber.java
@@ -1,1 +1,5 @@
 // Java Program to Find Number is Evil Or Odious
+
+// EvilOrOdiousNumber
+// Convert the given Number into binary format, If the binary representation of the number contains even 
+// number of 1's then its evil number or if contains odd number of 1's then its odious number
