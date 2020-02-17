abc72e703f6999694f0e67987e2fbc3b316e6e41
prabhusk <prabhu.sk.169@gmail.com>
2020-02-17 23:16:10 +0530

    Evil Odious static Method added.

--- Numbers/EvilOdiousNumber.java
+++ Numbers/EvilOdiousNumber.java
@@ -12,22 +12,49 @@ import java.util.Scanner;
 
 // Method - 1 (Using Logic)
 
+// public class EvilOdiousNumber {
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter the Number to Find Number is Evil or Odious Number : ");
+//         int number = input.nextInt();
+//         int temp = number;
+//         int count = 0;
+
+//         while(number != 0){
+//             if(number % 2 == 1)
+//                 count++;
+//             number /= 2;
+//         }
+
+//         System.out.println(count % 2 == 0 ? temp + " is Evil Number" : temp + " is Odious Number");
+//         input.close();
+//     }
+// }
+
+// Method - 2 (Using static method)
+
 public class EvilOdiousNumber {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Number to Find Number is Evil or Odious Number : ");
-        int number = input.nextInt();
-        int temp = number;
-        int count = 0;
+    public static int evilOdious(int number, int count){
 
         while(number != 0){
             if(number % 2 == 1)
                 count++;
             number /= 2;
         }
+        
+        return count;
+    }
 
-        System.out.println(count % 2 == 0 ? temp + " is Evil Number" : temp + " is Odious Number");
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Number is Evil Or Odious Number : ");
+        int number = input.nextInt();
+        int temp = number;
+        int count = 0;
+
+        System.out.println(evilOdious(number, count) % 2 == 0 ? temp + " is Evil Number" : temp + " is Odious Number");
         input.close();
     }
 }
