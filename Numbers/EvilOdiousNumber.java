a17201efbc482ad529f401245d782575e6a18331
prabhusk <prabhu.sk.169@gmail.com>
2020-02-17 22:37:13 +0530

    Evil Odious Logic added

--- Numbers/EvilOdiousNumber.java
+++ Numbers/EvilOdiousNumber.java
@@ -7,3 +7,27 @@
 // For Example :
 // 3 = 011 = Even number of 1's so its Evil Number
 // 16 = 10000 = Odd number of 1's so its Odious Number
+
+import java.util.Scanner;
+
+// Method - 1 (Using Logic)
+
+public class EvilOdiousNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Find Number is Evil or Odious Number : ");
+        int number = input.nextInt();
+        int temp = number;
+        int count = 0;
+
+        while(number != 0){
+            if(number % 2 == 1)
+                count++;
+            number /= 2;
+        }
+
+        System.out.println(count % 2 == 0 ? temp + " is Evil Number" : temp + " is Odious Number");
+        input.close();
+    }
+}
