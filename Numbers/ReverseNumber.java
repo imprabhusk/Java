f328045bfd1f32ccc324727a2a73b2a985b0b3a5
prabhusk <prabhu.sk.169@gmail.com>
2020-02-18 20:13:55 +0530

    Reverse number explanation added.

--- Numbers/ReverseNumber.java
+++ Numbers/ReverseNumber.java
@@ -3,3 +3,23 @@
 // For Example : 
 // 123 = 321
 // 1234 = 4321
+
+import java.util.Scanner;
+public class ReverseNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Reverse : ");
+        int number = input.nextInt();
+        int append = 0;
+
+        while(number != 0){
+            int remainder = number % 10;
+            append = (append * 10) + remainder;
+            number /= 10;
+        }
+
+        System.out.println("Reversed Number is : " + append);
+        input.close();
+    }
+}
