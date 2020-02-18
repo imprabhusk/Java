ce807e413a5b68b810741302ddf1813f73f1c7af
prabhusk <prabhu.sk.169@gmail.com>
2020-02-18 20:23:35 +0530

    Reverse number static method added.

--- Numbers/ReverseNumber.java
+++ Numbers/ReverseNumber.java
@@ -5,21 +5,47 @@
 // 1234 = 4321
 
 import java.util.Scanner;
+
+// Method - 1 (Using Logic)
+
+// public class ReverseNumber {
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter the Number to Reverse : ");
+//         int number = input.nextInt();
+//         int append = 0;
+
+//         while(number != 0){
+//             int remainder = number % 10;
+//             append = (append * 10) + remainder;
+//             number /= 10;
+//         }
+
+//         System.out.println("Reversed Number is : " + append);
+//         input.close();
+//     }
+// }
+
+// Method - 2 (Using static method)
+
 public class ReverseNumber {
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Number to Reverse : ");
-        int number = input.nextInt();
+    public static int reverse(int number){
         int append = 0;
-
         while(number != 0){
             int remainder = number % 10;
             append = (append * 10) + remainder;
             number /= 10;
         }
+        return append;
+    }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Number to Reverse : ");
+        int number = input.nextInt();
 
-        System.out.println("Reversed Number is : " + append);
+        System.out.println("Reversed Number is : " + reverse(number));
         input.close();
     }
 }
