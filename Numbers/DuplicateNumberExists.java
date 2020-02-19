9d7f61d509e4dc3e00a0cf8f7bdf8f9ac66a6f02
prabhusk <prabhu.sk.169@gmail.com>
2020-02-19 21:45:43 +0530

    Duplicate Number Exists explanation added.

--- Numbers/DuplicateNumberExists.java
+++ Numbers/DuplicateNumberExists.java
@@ -1,2 +1,5 @@
 // Java Program to Find Duplicate Numbers Exists in a given number or not.
 
+// Duplicate Numbers :
+// If the number is repeated again and again in the given number. we need to find the 
+// repeating number and its number of occurence
