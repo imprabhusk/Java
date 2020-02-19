82414a44d82e3f502d534a9e1c9321aed5474628
prabhusk <prabhu.sk.169@gmail.com>
2020-02-19 21:47:53 +0530

    Duplicate Number Exists example added.

--- Numbers/DuplicateNumberExists.java
+++ Numbers/DuplicateNumberExists.java
@@ -3,3 +3,10 @@
 // Duplicate Numbers :
 // If the number is repeated again and again in the given number. we need to find the 
 // repeating number and its number of occurence
+
+// For Example :
+// 123123 
+// Duplicate Numbers are 1, 2, 3
+// 1 = 2 times
+// 2 = 2 times
+// 3 = 2 times
