f2b54f1f3e44cb8474ce142351400903dfcb3a96
prabhusk <prabhu.sk.169@gmail.com>
2020-02-20 19:25:55 +0530

    Area of circle program added.

--- Area/Circle.java
+++ Area/Circle.java
@@ -1,1 +1,16 @@
-Java Program to Find Area of the Circle
+// Java Program to Find Area of the Circle
+
+// The Formula To Find Area of Circle is A = pi * r * r. where pi = 3.14
+
+import java.util.Scanner;
+
+public class Circle {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius of the Cicle to calculate Area : ");
+        int radius = input.nextInt();
+        System.out.println("Area of the Circle is : " + (float)3.14 * radius * radius);
+        input.close();
+    }
+}
