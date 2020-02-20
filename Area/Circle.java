085f6df57867f434ccd5426198e1fba8a1503e48
prabhusk <prabhu.sk.169@gmail.com>
2020-02-20 19:35:23 +0530

    Area of circle program added.

--- Area/Circle.java
+++ Area/Circle.java
@@ -4,13 +4,31 @@
 
 import java.util.Scanner;
 
+// Method - 1 (Using Logic)
+
+// public class Circle {
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter the radius of the Cicle to calculate Area : ");
+//         int radius = input.nextInt();
+//         System.out.println("Area of the Circle is : " + (float)3.14 * radius * radius);
+//         input.close();
+//     }
+// }
+
+// Method - 2 (Using static method)
+
 public class Circle {
+    public static float area(int radius){
+        return (float)3.14 * radius * radius;
+    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the radius of the Cicle to calculate Area : ");
+        System.out.print("Enter the radius to find Area of the Circle : ");
         int radius = input.nextInt();
-        System.out.println("Area of the Circle is : " + (float)3.14 * radius * radius);
+        System.out.print("Area of the Circle is : " + area(radius));
         input.close();
     }
 }
