589da0f725201df02b9e705c37aed8ca88bca7d6
prabhusk <prabhu.sk.169@gmail.com>
2020-04-19 19:21:58 +0530

    static methods added.

--- Area/Triangle.java
+++ Area/Triangle.java
@@ -1,15 +1,30 @@
 import java.util.Scanner;
 
+// public class Triangle {
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter the Length : ");
+//         float length = input.nextInt();
+//         System.out.print("Enter the Breadth : ");
+//         float height = input.nextInt();
+//         double area = (1/2) * (length * height);
+//         System.out.println("Area Of the triangle is : " + area);
+//         input.close();
+//     }
+// }
+
+// Method - 2 (Using static method)
+
 public class Triangle {
+    public static float area(float length, float height){
+        return (float)(1/2) * (length * height);
+    }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the Length : ");
-        int length = input.nextInt();
-        System.out.print("Enter the Breadth : ");
-        int breadth = input.nextInt();
-        double area = (1/2) * (length * breadth);
-        System.out.println("Area Of the triangle is : " + area);
-        input.close();
+        float length = input.nextInt();
+        float height = input.nextInt();
+        System.out.println("Area of the Triangle is " + area(length, height));
     }
 }
