59a45223c8279d439d67fd2f2eb85adcb9d7ff6f
prabhusk <prabhu.sk.169@gmail.com>
2020-04-19 19:10:26 +0530

    Area of Triangle added.

--- Area/Triangle.java
+++ Area/Triangle.java
@@ -8,7 +8,7 @@ public class Triangle {
         int length = input.nextInt();
         System.out.print("Enter the Breadth : ");
         int breadth = input.nextInt();
-        double area = 0.5 * (length * breadth);
+        double area = (1/2) * (length * breadth);
         System.out.println("Area Of the triangle is : " + area);
         input.close();
     }
