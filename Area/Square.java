41fbb8e417530b5eb136bfe0d4061817e3774d7a
prabhusk <prabhu.sk.169@gmail.com>
2020-04-25 21:03:01 +0530

    square added.

--- Area/Square.java
+++ Area/Square.java
@@ -2,47 +2,69 @@ import java.util.Scanner;
 
 // Method - 1 (Using Logic)
 
-public class Square {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the sides of the square : ");
-        double side = input.nextDouble();
+// public class Square {
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter the sides of the square : ");
+//         double side = input.nextDouble();
 
-        System.out.println("Area of the square is : " + side * side);
-        input.close();
-    }
-}
+//         System.out.println("Area of the square is : " + side * side);
+//         input.close();
+//     }
+// }
 
 // Method - 2 (Using static Method)
 
-public class Square {
-    public static double area(double side){
-        return side * side;
-    }
+// public class Square {
+//     public static double area(double side){
+//         return side * side;
+//     }
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("Enter the sides of the Square : ");
-        double side = input.nextDouble();
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter the sides of the Square : ");
+//         double side = input.nextDouble();
 
-        System.out.println("Area of the Square is : " + area(side));
-        input.close();
-    }
-}
+//         System.out.println("Area of the Square is : " + area(side));
+//         input.close();
+//     }
+// }
 
 // Method - 3 (Using Objects)
 
+// public class Square {
+//     public static double area(double side){
+//         return side * side;
+//     }
+
+//     public static void main(String[] args) {
+//         Scanner input = new Scanner(System.in);
+//         System.out.print("Enter the sides of the Square : ");
+//         double side = input.nextDouble();
+
+//         Square square = new Square();
+//         System.out.println("Area of the Square is : " + square.area(side));
+//         input.close();
+//     }
+// }
+
+// Method - 4 (Using Constructor)
+
 public class Square {
+    public Square(double side){
+        this.side = side;
+    }
+
     public static double area(double side){
         return side * side;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter the sides of the Square : ");
+        System.out.print("Enter the sides of the Squares : ");
         double side = input.nextDouble();
 
-        Square square = new Square();
+        Square square = new Square(side);
         System.out.println("Area of the Square is : " + square.area(side));
         input.close();
     }
