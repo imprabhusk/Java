fcab358c6e5425553d2253b8891c3d079b44e8e0
prabhusk <prabhu.sk.169@gmail.com>
2020-04-25 20:22:56 +0530

    square and rectangle added.

--- Area/Circle.java
+++ Area/Circle.java
@@ -6,44 +6,44 @@ import java.util.Scanner;
 
 // Method - 1 (Using Logic)
 
-// public class Circle {
+public class Circle {
 
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         System.out.print("Enter the radius to find Area of the Circle : ");
-//         int radius = input.nextInt();
-//         System.out.println("Area of the Circle is : " + (float)3.14 * radius * radius);
-//         input.close();
-//     }
-// }
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius to find Area of the Circle : ");
+        double radius = input.nextDouble();
+        System.out.println("Area of the Circle is : " + 3.14 * radius * radius);
+        input.close();
+    }
+}
 
 // Method - 2 (Using static method)
 
-// public class Circle {
-//     public static float area(int radius){
-//         return (float)3.14 * radius * radius;
-//     }
-
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         System.out.print("Enter the radius to find Area of the Circle : ");
-//         int radius = input.nextInt();
-//         System.out.print("Area of the Circle is : " + area(radius));
-//         input.close();
-//     }
-// }
-
-// Method - 3 (Using Objects)
-
 public class Circle {
-    public float area(int radius){
-        return (float)3.14 * radius * radius;
+    public static double area(double radius){
+        return 3.14 * radius * radius;
     }
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the radius to find Area of the Circle : ");
-        int radius = input.nextInt();
+        double radius = input.nextDouble();
+        System.out.print("Area of the Circle is : " + area(radius));
+        input.close();
+    }
+}
+
+// Method - 3 (Using Objects)
+
+public class Circle {
+    public double area(double radius){
+        return 3.14 * radius * radius;
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the radius to find Area of the Circle : ");
+        double radius = input.nextDouble();
 
         Circle cir = new Circle();
         System.out.println("Area of the Circle is : " + cir.area(radius));
