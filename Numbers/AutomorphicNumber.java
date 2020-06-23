6dfdd1a00d23d5ce8ac4c065b613e4e913c11b3d
prabhusk <prabhu.sk.169@gmail.com>
2020-06-24 00:02:49 +0530

    updated

--- Numbers/AutomorphicNumber.java
+++ Numbers/AutomorphicNumber.java
@@ -1,4 +1,4 @@
-// Java Program to Find Number is Strong Or Not
+// Java Program to Find Number is Automorphic Number Or Not
 
 // Automorphic Number :
 // The Number whose square ends with the same digit.
@@ -6,7 +6,7 @@
 // For Example :
 // 5 = 5 ^ 5 = 25 //Last digit is 5
 // 6 = 6 ^ 6 = 36 //Last digit is 6
-// 25 = 25 * 25 = 325 //Last digit is 25
+// 25 = 25 * 25 = 625 //Last digit is 25
 
 import java.util.Scanner;
 
