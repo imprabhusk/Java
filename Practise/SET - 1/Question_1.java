82d0eb0b7260baa5cd9d35ba4e24c3a48e283bde
prabhusk <prabhu.sk.169@gmail.com>
2021-01-01 22:05:19 +0530

    Question_1 added.

--- Practise/SET - 1/Question_1.java
+++ Practise/SET - 1/Question_1.java
@@ -1,1 +1,23 @@
 // Print the word with odd letters as cross type (X type)
+
+import java.util.Scanner;
+
+public class Question_1 {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the string with odd letters : ");
+        String pattern = input.next();
+
+        for (int i = 0; i < pattern.length(); i++) {
+            for (int j = 0; j < pattern.length(); j++) {
+                if (i == j || i + j == pattern.length() - 1)
+                    System.out.print(pattern.charAt(i));
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+        input.close();
+    }
+}
