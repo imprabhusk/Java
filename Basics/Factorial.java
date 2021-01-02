ab0d3ae3a17ada4d3bff3d4368622c1e614b0389
prabhusk <prabhu.sk.169@gmail.com>
2021-01-02 22:44:15 +0530

    Factorial program refactored

--- Basics/Factorial.java
+++ Basics/Factorial.java
@@ -4,58 +4,58 @@ import java.util.Scanner;
 
 // Method - 1 (using for loop)
 
-// public class Factorial {
+public class Factorial {
 
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         System.out.print("Enter the number to find factorial : ");
-//         int number = input.nextInt();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number to find factorial : ");
+        int number = input.nextInt();
 
-//         if (number < 0)
-//             System.out.println("Factorial of the negative number is undefined");
-//         else {
-//             int fact = 1;
-//             for (int i = 1; i <= number; i++) {
-//                 fact *= i;
-//             }
-//             System.out.println("Factorial of the number " + number + " is " + fact);
-//         }
-//         input.close();
-//     }
-// }
+        if (number < 0)
+            System.out.println("Factorial of the negative number is undefined");
+        else {
+            int fact = 1;
+            for (int i = 1; i <= number; i++) {
+                fact *= i;
+            }
+            System.out.println("Factorial of the number " + number + " is " + fact);
+        }
+        input.close();
+    }
+}
 
 // Method - 2 (Using while loop)
 
-// public class Factorial {
+public class Factorial {
 
-//     public static int factorial(int number) {
-//         int fact = 1;
-//         int i = 1;
+    public static int factorial(int number) {
+        int fact = 1;
+        int i = 1;
 
-//         if (number < 0)
-//             return -1;
-//         else {
-//             while (number > 0 && i <= number) {
-//                 fact *= i;
-//                 i++;
-//             }
-//         }
-//         return fact;
-//     }
+        if (number < 0)
+            return -1;
+        else {
+            while (number > 0 && i <= number) {
+                fact *= i;
+                i++;
+            }
+        }
+        return fact;
+    }
 
-//     public static void main(String[] args) {
-//         Scanner input = new Scanner(System.in);
-//         System.out.print("Enter the number to find factorial : ");
-//         int number = input.nextInt();
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the number to find factorial : ");
+        int number = input.nextInt();
 
-//         int result = factorial(number);
+        int result = factorial(number);
 
-//         System.out.println(result == -1 ? "Factorial of the negative number is undefined"
-//                 : "Factorial of the number " + number + " = " + result);
+        System.out.println(result == -1 ? "Factorial of the negative number is undefined"
+                : "Factorial of the number " + number + " = " + result);
 
-//         input.close();
-//     }
-// }
+        input.close();
+    }
+}
 
 // // Method - 3 (Using Recursion)
 
